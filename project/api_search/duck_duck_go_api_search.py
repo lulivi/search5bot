683eebe8e6c81b19bfc8644226167ad07f0b22bd
Jorge <jorge@localhost.localdomain>
2017-10-04 22:36:54 +0200

    Adapting format to comment conventions

--- project/api_search/duck_duck_go_api_search.py
+++ project/api_search/duck_duck_go_api_search.py
@@ -5,9 +5,14 @@ class DuckDuckGoApiSearch:
     __URL_TO_CALL = http://api.duckduckgo.com/?q=@TERM_TO_SEARCH@&format=json
 
     def search(term_to_search):
-        '''
+        """
         Return a list of results from DuckDuckGoApi
 
-        @param term_to_search The term to search
-        @return a list of results obtained by the API
-        '''
+        **Args:**
+
+        *term_to_search - The term to search
+
+        **Returns*
+
+        *List of results obtained by the API
+        """
