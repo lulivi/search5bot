6f3862f6415e34eda9643af95178de863144fcef
lulivi <luislivilla@gmail.com>
2017-10-08 18:12:05 +0200

    Cambiado formato de test y arreglados problemas de pep8

--- project/test/test_yandex_search_api.py
+++ project/test/test_yandex_search_api.py
@@ -1,34 +1,32 @@
+#!/usr/bin/env python
+"""Test batery for yandex_api_search module."""
+
 ######################################
 # test/test_yandex_search_api.py
 ######################################
 
-#!/usr/bin/env python
-
 # === Test yandex search api ===
-"""
-Test batery for yandex_api_search module
-"""
 
 # external imports
 import unittest
 import json
-imoprt requests
+import requests
 
 # path imports
 import __init__
 
 # internal imports
-import yandex_search_api
-
-from yandex_search_api import yandexSearchApi
+from yandex_search_api import YandexSearchApi
 
 
 class TestYandexSearchApi(unittest.TestCase):
     """Testing class for module yandex."""
 
     def test_get_search_resutls(self):
-        """Tests if the results are correct."""
-        yandex_s_a = yandex_search_api.FaroSearchApi()
-        url = 'http://www.yandex.com/api?q=iphone&start=1&length=10&l=en&src=web&f=json'
-        self.assertFalse(yandex_s_a.search_results(url)[1] == [], 'Not empty list')
-        self.assertTrue(yandex_s_a.search_results(url)[0] == 200, 'OK code')
+        """Test if the results are correct."""
+        search_item = 'iphone'
+        self.assertFalse(
+            YandexSearchApi.search_results(search_item)[1] == [],
+            'Not empty list')
+        self.assertTrue(
+            YandexSearchApi.search_results(search_item)[0] == 200, 'OK code')
