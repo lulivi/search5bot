d8670fac9f4cca254df8fb8898cd74cf4b512f1b
lulivi <luislivilla@gmail.com>
2017-10-08 23:22:40 +0200

    Using now decouple for api key

--- project/test/test_yandex_search_api.py
+++ project/test/test_yandex_search_api.py
@@ -9,11 +9,13 @@
 
 # external imports
 import unittest
+from decouple import config
 
 # path imports
-import __init__
+import __init__ as init
 
 # internal imports
+import settings
 from yandex_search_api import YandexSearchApi
 
 
@@ -23,8 +25,8 @@ class TestYandexSearchApi(unittest.TestCase):
     def test_get_search_resutls(self):
         """Test if the results are correct."""
         search_item = 'hola'
-        yandex_sa = YandexSearchApi(__init__._YANDEX_API_USER,
-                                    __init__._YANDEX_API_KEY)
+        yandex_sa = YandexSearchApi(config('YANDEX_API_USER'),
+                                    config('YANDEX_API_KEY'))
         self.assertFalse(
             yandex_sa.get_search_result(search_item) == [],
             'Not empty list')
