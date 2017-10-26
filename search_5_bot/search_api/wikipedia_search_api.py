1212a1c3f48b45136430dcb4dc50ca745a647a49
lulivi <luislivilla@gmail.com>
2017-10-27 01:44:56 +0200

    Added wikipedia search api

---
+++ search_5_bot/search_api/wikipedia_search_api.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""wikipedia_search_api module."""
+import wikipedia
+import re
+
+from json import dumps
+
+# local imports
+import generic_search_api
+
+
+class WikipediaSearchApi(generic_search_api.GenericSearchApi):
+    """Represent an API to get information from Wikipedia."""
+
+    __base_url = 'https://{}.wikipedia.org/wiki/{}'
+
+    def __init__(self):
+        """Instantiate the DuckDuckgo class."""
+        pass
+
+    def get_search_results(self, keywords, language):
+        """
+        Send a HTML request and save results into a list of dictionaries.
+
+        **Args:**
+
+        * keywords - Search terms
+
+        **Returns:**
+
+        * List of dictionaries from JSON that contains a title, a description
+        and an url
+        """
+        search_results = list()
+        try:
+            wikipedia.set_lang(language)
+        except Exception as e:
+            raise
+        else:
+            title_results = wikipedia.search(query=keywords, results=5)
+            for page_title in title_results:
+
+                new_search_field = dict()
+
+                try:
+                    # Try to get the page
+                    page_object = wikipedia.page(title=page_title)
+
+                except wikipedia.exceptions.PageError as e:
+
+                    raise
+
+                else:
+
+                    # Result title
+                    title = page_title.encode('utf-8').decode('utf-8')
+                    new_search_field['title'] = title
+
+                    # Result description
+                    description = page_object.summary[:100] + '...'
+                    new_search_field['descrtiption'] = description.encode(
+                        'utf-8').decode('utf-8')
+
+                    # URL
+                    title_to_url = re.sub(r'( )', '_', title)
+                    url = self.__base_url.format(language, title_to_url)
+                    new_search_field['url'] = url
+
+                    search_results.append(new_search_field)
+
+            return search_results
+
