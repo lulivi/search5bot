bff333704911b3b253eed6dc90469afc62fa5bc6
lulivi <luislivilla@gmail.com>
2017-10-30 02:35:07 +0100

    Forget the wikipedia python api and start using the oficial wikipedia api with json responses

--- search_5_bot/search_api/wikipedia_search_api.py
+++ search_5_bot/search_api/wikipedia_search_api.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 """wikipedia_search_api module."""
-import wikipedia
 import re
+import json
+import requests
+from urllib.parse import unquote_plus as unquote_url
 
 # local imports
 import generic_search_api
@@ -10,60 +12,139 @@ import generic_search_api
 class WikipediaSearchApi(generic_search_api.GenericSearchApi):
     """Represent an API to get information from Wikipedia."""
 
-    __base_url = 'https://{}.wikipedia.org/wiki/{}'
-
     def __init__(self):
-        """Instantiate the DuckDuckgo class."""
+        """Instantiate the WikipediaSearchApi class."""
+        self.__actual_language = ''
+        self.__actual_url = 'https://{}.wikipedia.org/w/api.php'
+        self.__base_url = 'https://{}.wikipedia.org/w/api.php'
         pass
+
+    def _set_lang(self, language):
+        """
+        Set the language for the next search.
+
+        Sets the current language and insert the language in the base url to
+        get the actual url
+
+        Args:
+
+        * language - search langauge
+        """
+        self.__actual_language = language
+        self.__actual_url = self.__base_url.format(language)
 
     def get_search_results(self, keywords, language):
         """
-        Send a HTML request and save results into a list of dictionaries.
+        Get a search from wikipedia.
 
-        **Args:**
+        Performs a request to search for a page called `keywords` with
+        underscores.
+
+        Args:
 
         * keywords - Search terms
         * language - Search language
 
-        **Returns:**
+        Returns:
 
-        * List of dictionaries from JSON that contains a title, a description
+        * List of dictionaries from JSON that contain a title, a description
         and an url
         """
+        # Set api language
+        if self.__actual_language != language:
+            self._set_lang(language)
+
+        # Get the keywords with underscores instead of spaces for the url
+        search_keyword = re.sub(r'( )', '_', keywords)
+
+        params = dict(
+
+            # The opensearch protocol
+            action='opensearch',
+
+            # Our actuals search keywords
+            search=search_keyword,
+
+            # 5 + 1 because the first item in desambiguation is the
+            # desambiguation itself
+            limit='6',
+
+            # Namespace of articles
+            namespace='0',
+
+            # Format of the request
+            format='json')
+
+        # Perform the main request
+        response = requests.get(url=self.__actual_url, params=params)
+
+        # Create a list for the entries
         search_results = list()
-        try:
-            wikipedia.set_lang(language)
-        except Exception as e:
-            raise
-        else:
-            title_results = wikipedia.search(query=keywords, results=5)
-            for page_title in title_results:
 
-                new_search_field = dict()
+        # No errors
+        if response.ok:
 
-                try:
-                    # Try to get the page
-                    page_object = wikipedia.page(title=page_title)
+            # Load the content of the request into a dictionary
+            content = json.loads(response.content)
 
-                except wikipedia.exceptions.PageError as e:
+            # Valid url page
+            if content[2] != []:
 
-                    raise
+                # Existent url page
+                if content[2][0] != '':
 
+                    # Article page
+                    if len(content[1]) == 1:
+
+                        new_search_field = dict()
+
+                        # Title
+                        title = content[1][0]
+                        new_search_field['title'] = title
+
+                        # Description
+                        description = content[2][0]
+                        new_search_field['description'] = description
+
+                        # URL
+                        url = content[3][0]
+                        new_search_field['url'] = unquote_url(url)
+
+                        search_results.append(new_search_field)
+
+                    # Desambiguation page
+                    else:
+
+                        # From the second item because the first one is the
+                        # desambiguation itself
+                        for actual_item in range(1, len(content[1])):
+
+                            new_search_field = dict()
+
+                            # Title
+                            title = content[1][actual_item]
+                            new_search_field['title'] = title
+
+                            # Description
+                            description = content[2][actual_item]
+                            new_search_field['description'] = description
+
+                            # URL
+                            url = content[3][actual_item]
+                            new_search_field['url'] = unquote_url(url)
+
+                            search_results.append(new_search_field)
+
+                # Redirect url page
                 else:
+                    pass
 
-                    # Result title
-                    title = page_title
-                    new_search_field['title'] = title
+            # Non valid url page
+            else:
+                pass
 
-                    # Result description
-                    description = page_object.summary[:100] + '...'
-                    new_search_field['description'] = description
+        # Page not existent
+        else:
+            pass
 
-                    # URL
-                    title_to_url = re.sub(r'( )', '_', title)
-                    url = self.__base_url.format(language, title_to_url)
-                    new_search_field['url'] = url
-
-                    search_results.append(new_search_field)
-
-            return search_results
+        return search_results
